48be093597232c648e0063923d6d01d1b292a30a
ytgon <baraaris2@gmail.com>
2024-01-23 07:33:13 +0800

    This file/code file contain FastAPI package for running at local server

---
+++ app.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+from router import router
+from fastapi.staticfiles import StaticFiles
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.include_router(router=router)
